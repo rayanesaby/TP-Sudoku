64d5af55620be30f71124b10bc38811676582790
theomercier <theomercier@icloud.com>
2020-01-14 16:53:31 +0100

    Start
    Mise en route par le prof

---
+++ genetic_solver/GeneticSolver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace genetic_solver
+{
+    public class GeneticSolver
+    {
+        public GeneticSolver()
+        {
+        }
+    }
+}
---
+++ genetic_solver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace genetic_solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
