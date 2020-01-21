aaf7dc65e7999bd2fcc4e08467a338148f86cec0
clemriv <clementrivet22@gmail.com>
2020-01-21 14:57:54 +0100

    mise à niveau après passage au tableau

--- Z3Solver/Program.cs
+++ Z3Solver/Program.cs
@@ -14,70 +14,73 @@ namespace Z3Solver
                 SudokuExample(ctx);
             }
 
-            static void SudokuExample(Context ctx)
+            
+
+        }
+        static void SudokuExample(Context ctx)
+        {
+            Console.WriteLine("SudokuExample");
+
+            // 9x9 matrix of integer variables
+            IntExpr[][] X = new IntExpr[9][];
+            for (uint i = 0; i < 9; i++)
             {
-                Console.WriteLine("SudokuExample");
-
-                // 9x9 matrix of integer variables
-                IntExpr[][] X = new IntExpr[9][];
-                for (uint i = 0; i < 9; i++)
-                {
-                    X[i] = new IntExpr[9];
-                    for (uint j = 0; j < 9; j++)
-                        X[i][j] = (IntExpr)ctx.MkConst(ctx.MkSymbol("x_" + (i + 1) + "_" + (j + 1)), ctx.IntSort);
-                }
-
-                // each cell contains a value in {1, ..., 9}
-                Expr[][] cells_c = new Expr[9][];
-                for (uint i = 0; i < 9; i++)
-                {
-                    cells_c[i] = new BoolExpr[9];
-                    for (uint j = 0; j < 9; j++)
-                        cells_c[i][j] = ctx.MkAnd(ctx.MkLe(ctx.MkInt(1), X[i][j]),
-                                                  ctx.MkLe(X[i][j], ctx.MkInt(9)));
-                }
-
-                // each row contains a digit at most once
-                BoolExpr[] rows_c = new BoolExpr[9];
-                for (uint i = 0; i < 9; i++)
-                    rows_c[i] = ctx.MkDistinct(X[i]);
-
-                // each column contains a digit at most once
-                BoolExpr[] cols_c = new BoolExpr[9];
+                X[i] = new IntExpr[9];
                 for (uint j = 0; j < 9; j++)
+                    X[i][j] = (IntExpr)ctx.MkConst(ctx.MkSymbol("x_" + (i + 1) + "_" + (j + 1)), ctx.IntSort);
+            }
+
+            // each cell contains a value in {1, ..., 9}
+            Expr[][] cells_c = new Expr[9][];
+            for (uint i = 0; i < 9; i++)
+            {
+                cells_c[i] = new BoolExpr[9];
+                for (uint j = 0; j < 9; j++)
+                    cells_c[i][j] = ctx.MkAnd(ctx.MkLe(ctx.MkInt(1), X[i][j]),
+                                              ctx.MkLe(X[i][j], ctx.MkInt(9)));
+            }
+
+            // each row contains a digit at most once
+            BoolExpr[] rows_c = new BoolExpr[9];
+            for (uint i = 0; i < 9; i++)
+                rows_c[i] = ctx.MkDistinct(X[i]);
+
+            // each column contains a digit at most once
+            BoolExpr[] cols_c = new BoolExpr[9];
+            for (uint j = 0; j < 9; j++)
+            {
+                IntExpr[] column = new IntExpr[9];
+                for (uint i = 0; i < 9; i++)
+                    column[i] = X[i][j];
+
+                cols_c[j] = ctx.MkDistinct(column);
+            }
+
+            // each 3x3 square contains a digit at most once
+            BoolExpr[][] sq_c = new BoolExpr[3][];
+            for (uint i0 = 0; i0 < 3; i0++)
+            {
+                sq_c[i0] = new BoolExpr[3];
+                for (uint j0 = 0; j0 < 3; j0++)
                 {
-                    IntExpr[] column = new IntExpr[9];
-                    for (uint i = 0; i < 9; i++)
-                        column[i] = X[i][j];
-
-                    cols_c[j] = ctx.MkDistinct(column);
+                    IntExpr[] square = new IntExpr[9];
+                    for (uint i = 0; i < 3; i++)
+                        for (uint j = 0; j < 3; j++)
+                            square[3 * i + j] = X[3 * i0 + i][3 * j0 + j];
+                    sq_c[i0][j0] = ctx.MkDistinct(square);
                 }
+            }
 
-                // each 3x3 square contains a digit at most once
-                BoolExpr[][] sq_c = new BoolExpr[3][];
-                for (uint i0 = 0; i0 < 3; i0++)
-                {
-                    sq_c[i0] = new BoolExpr[3];
-                    for (uint j0 = 0; j0 < 3; j0++)
-                    {
-                        IntExpr[] square = new IntExpr[9];
-                        for (uint i = 0; i < 3; i++)
-                            for (uint j = 0; j < 3; j++)
-                                square[3 * i + j] = X[3 * i0 + i][3 * j0 + j];
-                        sq_c[i0][j0] = ctx.MkDistinct(square);
-                    }
-                }
+            BoolExpr sudoku_c = ctx.MkTrue();
+            foreach (BoolExpr[] t in cells_c)
+                sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
+            sudoku_c = ctx.MkAnd(ctx.MkAnd(rows_c), sudoku_c);
+            sudoku_c = ctx.MkAnd(ctx.MkAnd(cols_c), sudoku_c);
+            foreach (BoolExpr[] t in sq_c)
+                sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
 
-                BoolExpr sudoku_c = ctx.MkTrue();
-                foreach (BoolExpr[] t in cells_c)
-                    sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
-                sudoku_c = ctx.MkAnd(ctx.MkAnd(rows_c), sudoku_c);
-                sudoku_c = ctx.MkAnd(ctx.MkAnd(cols_c), sudoku_c);
-                foreach (BoolExpr[] t in sq_c)
-                    sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
-
-                // sudoku instance, we use '0' for empty cells
-                int[,] instance = {{0,0,0,0,9,4,0,3,0},
+            // sudoku instance, we use '0' for empty cells
+            int[,] instance = {{0,0,0,0,9,4,0,3,0},
                                {0,0,0,5,1,0,0,0,7},
                                {0,8,9,0,0,0,0,4,0},
                                {0,0,0,0,0,0,2,0,8},
@@ -87,41 +90,39 @@ namespace Z3Solver
                                {9,0,0,0,6,5,0,0,0},
                                {0,4,0,9,7,0,0,0,0}};
 
-                BoolExpr instance_c = ctx.MkTrue();
+            BoolExpr instance_c = ctx.MkTrue();
+            for (uint i = 0; i < 9; i++)
+                for (uint j = 0; j < 9; j++)
+                    instance_c = ctx.MkAnd(instance_c,
+                        (BoolExpr)
+                        ctx.MkITE(ctx.MkEq(ctx.MkInt(instance[i, j]), ctx.MkInt(0)),
+                                    ctx.MkTrue(),
+                                    ctx.MkEq(X[i][j], ctx.MkInt(instance[i, j]))));
+
+            Solver s = ctx.MkSolver();
+            s.Assert(sudoku_c);
+            s.Assert(instance_c);
+
+            if (s.Check() == Status.SATISFIABLE)
+            {
+                Model m = s.Model;
+                Expr[,] R = new Expr[9, 9];
                 for (uint i = 0; i < 9; i++)
                     for (uint j = 0; j < 9; j++)
-                        instance_c = ctx.MkAnd(instance_c,
-                            (BoolExpr)
-                            ctx.MkITE(ctx.MkEq(ctx.MkInt(instance[i, j]), ctx.MkInt(0)),
-                                        ctx.MkTrue(),
-                                        ctx.MkEq(X[i][j], ctx.MkInt(instance[i, j]))));
-
-                Solver s = ctx.MkSolver();
-                s.Assert(sudoku_c);
-                s.Assert(instance_c);
-
-                if (s.Check() == Status.SATISFIABLE)
+                        R[i, j] = m.Evaluate(X[i][j]);
+                Console.WriteLine("Sudoku solution:");
+                for (uint i = 0; i < 9; i++)
                 {
-                    Model m = s.Model;
-                    Expr[,] R = new Expr[9, 9];
-                    for (uint i = 0; i < 9; i++)
-                        for (uint j = 0; j < 9; j++)
-                            R[i, j] = m.Evaluate(X[i][j]);
-                    Console.WriteLine("Sudoku solution:");
-                    for (uint i = 0; i < 9; i++)
-                    {
-                        for (uint j = 0; j < 9; j++)
-                            Console.Write(" " + R[i, j]);
-                        Console.WriteLine();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Failed to solve sudoku");
-
+                    for (uint j = 0; j < 9; j++)
+                        Console.Write(" " + R[i, j]);
+                    Console.WriteLine();
                 }
             }
+            else
+            {
+                Console.WriteLine("Failed to solve sudoku");
 
+            }
         }
     }
 }
--- Z3Solver/Z3Solver.cs
+++ Z3Solver/Z3Solver.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Microsoft.Z3;
 using Noyau;
 
 namespace Z3Solver
@@ -9,7 +11,99 @@ namespace Z3Solver
     {
         public Sudoku Solve(Sudoku s)
         {
-            throw new NotImplementedException();
+            using (Context ctx = new Context(new Dictionary<string, string>() { { "model", "true" } }))
+            {
+                // 9x9 matrix of integer variables
+                IntExpr[][] X = new IntExpr[9][];
+                for (uint i = 0; i < 9; i++)
+                {
+                    X[i] = new IntExpr[9];
+                    for (uint j = 0; j < 9; j++)
+                        X[i][j] = (IntExpr)ctx.MkConst(ctx.MkSymbol("x_" + (i + 1) + "_" + (j + 1)), ctx.IntSort);
+                }
+
+                // each cell contains a value in {1, ..., 9}
+                Expr[][] cells_c = new Expr[9][];
+                for (uint i = 0; i < 9; i++)
+                {
+                    cells_c[i] = new BoolExpr[9];
+                    for (uint j = 0; j < 9; j++)
+                        cells_c[i][j] = ctx.MkAnd(ctx.MkLe(ctx.MkInt(1), X[i][j]),
+                                                  ctx.MkLe(X[i][j], ctx.MkInt(9)));
+                }
+
+                // each row contains a digit at most once
+                BoolExpr[] rows_c = new BoolExpr[9];
+                for (uint i = 0; i < 9; i++)
+                    rows_c[i] = ctx.MkDistinct(X[i]);
+
+                // each column contains a digit at most once
+                BoolExpr[] cols_c = new BoolExpr[9];
+                for (uint j = 0; j < 9; j++)
+                {
+                    IntExpr[] column = new IntExpr[9];
+                    for (uint i = 0; i < 9; i++)
+                        column[i] = X[i][j];
+
+                    cols_c[j] = ctx.MkDistinct(column);
+                }
+
+                // each 3x3 square contains a digit at most once
+                BoolExpr[][] sq_c = new BoolExpr[3][];
+                for (uint i0 = 0; i0 < 3; i0++)
+                {
+                    sq_c[i0] = new BoolExpr[3];
+                    for (uint j0 = 0; j0 < 3; j0++)
+                    {
+                        IntExpr[] square = new IntExpr[9];
+                        for (uint i = 0; i < 3; i++)
+                            for (uint j = 0; j < 3; j++)
+                                square[3 * i + j] = X[3 * i0 + i][3 * j0 + j];
+                        sq_c[i0][j0] = ctx.MkDistinct(square);
+                    }
+                }
+
+                BoolExpr sudoku_c = ctx.MkTrue();
+                foreach (BoolExpr[] t in cells_c)
+                    sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
+                sudoku_c = ctx.MkAnd(ctx.MkAnd(rows_c), sudoku_c);
+                sudoku_c = ctx.MkAnd(ctx.MkAnd(cols_c), sudoku_c);
+                foreach (BoolExpr[] t in sq_c)
+                    sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
+
+                // sudoku instance, we use '0' for empty cells
+                var listCell = new List<List<int>>();
+                for (int i = 0; i < 9; i++)
+                {
+                    listCell.Add(new List<int>());
+                    for (int j = 0; j < 9; j++)
+                    {
+                        listCell[listCell.Count - 1].Add(s.Cells[9 * i + j]);
+                    }
+                }
+                int[,] instance = To2D(listCell.Select(ligne => ligne.ToArray()).ToArray()); 
+
+            }
+        }
+
+        static T[,] To2D<T>(T[][] source)
+        {
+            try
+            {
+                int FirstDim = source.Length;
+                int SecondDim = source.GroupBy(row => row.Length).Single().Key; // throws InvalidOperationException if source is not rectangular
+
+                var result = new T[FirstDim, SecondDim];
+                for (int i = 0; i < FirstDim; ++i)
+                    for (int j = 0; j < SecondDim; ++j)
+                        result[i, j] = source[i][j];
+
+                return result;
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException("The given jagged array is not rectangular.");
+            }
         }
     }
 }
