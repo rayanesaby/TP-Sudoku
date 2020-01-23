c36e342e058f08b665d41b0cb1bfa093bd1be2a6
rayan <rayan@DESKTOP-S3F4PT5>
2020-01-23 17:57:14 +0100

    Préparation test pour les autres solution de résolution de sudoku

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -34,6 +34,15 @@ namespace Benchmark
             Noyau.Sudoku s = new Noyau.Sudoku(initial_grid);
             Noyau.Sudoku s_1 = new Noyau.Sudoku();
             Noyau.Sudoku s_2 = new Noyau.Sudoku();
+            Noyau.Sudoku s_3 = new Noyau.Sudoku();
+            Noyau.Sudoku s_4 = new Noyau.Sudoku();
+            Noyau.Sudoku s_5 = new Noyau.Sudoku();
+            Noyau.Sudoku s_6 = new Noyau.Sudoku();
+            Noyau.Sudoku s_7 = new Noyau.Sudoku();
+
+
+
+
 
             var fitness = new SudokuFitness(s);
 
@@ -95,22 +104,131 @@ namespace Benchmark
             Console.WriteLine("\n");
 
             //instruction next algo
+            Console.WriteLine("Résolution par CSP ");
+
+            //CSP csp = new CSP();
+
+            //chrono start
+            stopwatch.Start();
+
+            //s_3 = csp.Solve(s);
+
+            //chrono stop
+            stopwatch.Stop();
+            Console.WriteLine(s_3.ToString());
+
+            //fonction pour evaluer si un sudoku est bon : objectif 0
+            Console.WriteLine("Fitness : ");
+            Console.WriteLine(fitness.Evaluate(s_3));
+
+            //instruction durée d'exe
+            Console.WriteLine("Durée d'exécution: {0} secondes", stopwatch.Elapsed.TotalSeconds);
+            stopwatch.Reset();
+
 
             Console.WriteLine("\n");
             Console.WriteLine("******************************************************");
             Console.WriteLine("\n");
 
             //instruction next algo
+            Console.WriteLine("Résolution par SMT  ");
+
+            //SMT smt = new SMT();
+
+            //chrono start
+            stopwatch.Start();
+
+            //s_4 = smt.Solve(s);
+
+            //chrono stop
+            stopwatch.Stop();
+            Console.WriteLine(s_4.ToString());
+
+            //fonction pour evaluer si un sudoku est bon : objectif 0
+            Console.WriteLine("Fitness : ");
+            Console.WriteLine(fitness.Evaluate(s_4));
+
+            //instruction durée d'exe
+            Console.WriteLine("Durée d'exécution: {0} secondes", stopwatch.Elapsed.TotalSeconds);
+            stopwatch.Reset();
 
             Console.WriteLine("\n");
             Console.WriteLine("******************************************************");
             Console.WriteLine("\n");
 
             //instruction next algo
+            Console.WriteLine("Résolution par Dancing_Links  ");
+
+            //Dancing_Links dancing = new Dancing_Links();
+
+            //chrono start
+            stopwatch.Start();
+
+            //s_5 = dancing.Solve(s);
+
+            //chrono stop
+            stopwatch.Stop();
+            Console.WriteLine(s_5.ToString());
+
+            //fonction pour evaluer si un sudoku est bon : objectif 0
+            Console.WriteLine("Fitness : ");
+            Console.WriteLine(fitness.Evaluate(s_5));
+
+            //instruction durée d'exe
+            Console.WriteLine("Durée d'exécution: {0} secondes", stopwatch.Elapsed.TotalSeconds);
+            stopwatch.Reset();
 
             Console.WriteLine("\n");
             Console.WriteLine("******************************************************");
             Console.WriteLine("\n");
+
+            //instruction next algo
+            Console.WriteLine("Résolution à la Norvig ");
+
+            //Norvig nor = new Norvig();
+
+            //chrono start
+            stopwatch.Start();
+
+            //s_6 = nor.Solve(s);
+
+            //chrono stop
+            stopwatch.Stop();
+            Console.WriteLine(s_6.ToString());
+
+            //fonction pour evaluer si un sudoku est bon : objectif 0
+            Console.WriteLine("Fitness : ");
+            Console.WriteLine(fitness.Evaluate(s_6));
+
+            //instruction durée d'exe
+            Console.WriteLine("Durée d'exécution: {0} secondes", stopwatch.Elapsed.TotalSeconds);
+            stopwatch.Reset();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("\n");
+
+            //instruction next algo
+            Console.WriteLine(" Résolution par réseau de neurones convolués ");
+
+            //Neurones neur = new Neurones();
+
+            //chrono start
+            stopwatch.Start();
+
+            //s_7 = neur.Solve(s);
+
+            //chrono stop
+            stopwatch.Stop();
+            Console.WriteLine(s_7.ToString());
+
+            //fonction pour evaluer si un sudoku est bon : objectif 0
+            Console.WriteLine("Fitness : ");
+            Console.WriteLine(fitness.Evaluate(s_7));
+
+            //instruction durée d'exe
+            Console.WriteLine("Durée d'exécution: {0} secondes", stopwatch.Elapsed.TotalSeconds);
+            stopwatch.Reset();
 
             Console.ReadLine();
         }
