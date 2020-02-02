07d47197372fc590864f1253a9a004cf3171bd83
clemriv <clementrivet22@gmail.com>
2020-02-02 15:26:07 +0100

    Test d'intégration du script python, sans succès

--- Z3Solver/Program.cs
+++ Z3Solver/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using Noyau;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
 
 
 namespace Z3Solver
@@ -8,7 +11,9 @@ namespace Z3Solver
     {
         static void Main(string[] args)
         {
+            // Résoltuion avec C# 
 
+            // On créé la grille de Sudoku à résoudre
             int[] initial_grid = new int[] { 0, 6, 0, 0, 5, 0, 0, 2, 0,
 
                                              0, 0, 0, 3, 0, 0, 0, 9, 0,
@@ -27,8 +32,10 @@ namespace Z3Solver
 
                                              0, 2, 0, 0, 4, 0, 0, 5, 0 };
 
+            // Pour être conforme au groupe arbitre on créé "toSolve"
             Noyau.Sudoku toSolve = new Sudoku(initial_grid);
             Console.WriteLine(toSolve.ToString());
+
 
             Z3Solver solve = new Z3Solver();
 
@@ -42,8 +49,14 @@ namespace Z3Solver
 
             Console.WriteLine(toSolve.ToString());
 
-            Console.WriteLine("La solution a été trouvé en " + (double)t_exec.TotalSeconds + "secondes");
+            Console.WriteLine("La solution a été trouvé en " + (double)t_exec.TotalSeconds + " secondes");
 
+            // Résoltuion avec Python en utilisant Ironpython
+
+            string current_path = Directory.GetCurrentDirectory();
+            string py_path = @"..\..\..\..\solve_using_z3.py";
+            ScriptEngine engine = Python.CreateEngine();
+            engine.ExecuteFile(py_path);
         }
 
     }
