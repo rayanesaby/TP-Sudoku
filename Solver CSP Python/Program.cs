5ee0e456676d33c5b398d490a0cd9b4393b8803f
Dan <Dan@DESKTOP-LV6DF4U>
2020-02-03 19:12:57 +0100

    modification sur CSP SolverPython en commentaire

---
+++ Solver CSP Python/Program.cs
@@ -0,0 +1,40 @@
+﻿using Solver_CSP_Python;
+using System;
+
+namespace ORToolsSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] initial_grid = new int[] { 0, 6, 0, 0, 5, 0, 0, 2, 0,
+
+                                             0, 0, 0, 3, 0, 0, 0, 9, 0,
+
+                                             7, 0, 0, 6, 0, 0, 0, 1, 0,
+
+                                             0, 0, 6, 0, 3, 0, 4, 0, 0,
+
+                                             0, 0, 4, 0, 7, 0, 1, 0, 0,
+
+                                             0, 0, 5, 0, 9, 0, 8, 0, 0,
+
+                                             0, 4, 0, 0, 0, 1, 0, 0, 6,
+
+                                             0, 3, 0, 0, 0, 8, 0, 0, 0,
+
+                                             0, 2, 0, 0, 4, 0, 0, 5, 0 };
+
+
+            Noyau.Sudoku s = new Noyau.Sudoku(initial_grid);
+            Console.WriteLine(s.ToString());
+
+            SolverCSPPython ORT = new SolverCSPPython();
+            s = ORT.Solve(s);
+            Console.WriteLine(s.ToString());
+
+
+
+        }
+    }
+}
---
+++ Solver CSP Python/SolverCSPPython.cs
@@ -0,0 +1,38 @@
+﻿using Noyau;
+using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solver_CSP_Python
+{
+    public class SolverCSPPython : ISudokuSolver
+    {
+        public Sudoku Solve(Sudoku s)
+        {
+
+            using (Py.GIL())
+            {
+
+                using (PyScope scope = Py.CreateScope())
+                {
+                    // convert the sudoku object to a PyObject
+                    PyObject pySudoku = s.ToPython();
+
+                    // create a Python variable "sudoku"
+                    scope.Set("sudoku", pySudoku);
+
+                    // the sudoku object may now be used in Python
+                    string code = "fullName = person.FirstName + ' ' + person.LastName";
+                    scope.Exec(code);
+                    s = scope.Get<Sudoku>("sudoku");
+                }
+
+
+                return s;
+                // PythonEngine.Exec("doStuff()");
+            }
+
+        }
+    }
+}
