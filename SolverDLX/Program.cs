55b2be19ab894c55346bd152ac902418e6af5b2a
magdaleenaaaa <59876908+magdaleenaaaa@users.noreply.github.com>
2020-02-04 13:03:38 +0100

    Solver dlx

---
+++ SolverDLX/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SolverDLX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allSudokus = Sudoku.Core.Sudoku.ParseFile("../../data/Sudoku_Easy50.txt");
+            var SudokuSolver = new SolverDlx();
+            Console.Write(allSudokus[1]);
+            var resolu = SudokuSolver.Solve(allSudokus[1]);
+            Console.WriteLine(resolu.ToString());
+        }
+    }
+}
